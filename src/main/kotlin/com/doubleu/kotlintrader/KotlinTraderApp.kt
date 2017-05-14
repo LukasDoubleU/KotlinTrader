d1e28c82711fa2db08008b2500359e14c7a1b23f
Lukas Wulff <lukaswulff@gmx.net>
2017-05-14 22:13:04 +0200

    Added Dokka docs

--- src/main/kotlin/com/doubleu/kotlintrader/KotlinTraderApp.kt
+++ src/main/kotlin/com/doubleu/kotlintrader/KotlinTraderApp.kt
@@ -7,6 +7,7 @@ import tornadofx.*
 // TODO implement Trade- and MasterController
 // TODO allow for creation of new entity objects
 // TODO ReferenceDelegates can not yet reference RefEntities
+// TODO use JavaFX-Gradle-Plugin for JAR creation
 // TODO remove files ignored by .gitignore from GitHub
 
 class KotlinTraderApp : App(MainView::class) {
